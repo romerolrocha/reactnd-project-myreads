619b20db4d51349402acb6b9440d67f4c934127c
Claudio Romero <romerolrocha@gmail.com>
2017-08-23 00:02:18 -0300

    Remover código estático desnecessário

--- src/App.js
+++ src/App.js
@@ -2,25 +2,15 @@ import React from 'react'
 import * as BooksAPI from './data/BooksAPI'
 import './css/App.css'
 import AddBook from './AddBook'
-import BookList from './BookList'
+import BookShelf from './BookShelf'
 import { Route } from 'react-router-dom'
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: true
-  }
-
   render() {
     return (
       <div className="app">
         <Route exact path='/' render={() => (
-          <BookList />
+          <BookShelf />
         )}/>
         <Route path='/add' render={() => (
           <AddBook />
